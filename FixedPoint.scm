a1dc26d3e1f1389546f7dc9a10a6e5afacb24e85
Dedipyaman <30053071+2Dsharp@users.noreply.github.com>
2018-04-20 21:06:49 +0530

    Abstracted tolerance and removed parameter for tolerance

--- FixedPoint.scm
+++ FixedPoint.scm
@@ -1,11 +1,11 @@
 ;; SICP- Second Edition
 ;; Author- Dedipyaman Das (2d@twodee.me)
 ;; Fixed point function
-
+(define tolerance 0.00001)
 (define (fixed-point f guess)
-  (define (close-enough? guess new-guess tolerance)
+  (define (close-enough? guess new-guess)
     (< (abs (- guess new-guess)) tolerance))
   (let ((new-guess (f guess)))
-    (if (close-enough? guess new-guess 0.00001)
+    (if (close-enough? guess new-guess)
 	new-guess
 	(fixed-point f new-guess))))
